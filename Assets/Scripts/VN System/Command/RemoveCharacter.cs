c586358c5f9ef63f2320d9424b04250a9af1e1e9
maurigalvez <mauri.galvez@hotmail.com>
2021-02-06 14:04:32 -0500

    + adding Character_Exit animation + adding script 4d

--- Assets/Scripts/VN System/Command/RemoveCharacter.cs
+++ Assets/Scripts/VN System/Command/RemoveCharacter.cs
@@ -12,13 +12,26 @@ namespace Logic
         /// <summary>
         /// Name of character to display
         /// </summary>
-        [SerializeField]
-        private string m_CharacterName = "";
+        [SerializeField] private string m_CharacterName = "";
+        [SerializeField] private bool m_Async = false;
+
+        public override void Enter()
+        {
+            base.Enter();
+            DisplayManager.instance.RemoveCharacter(m_CharacterName);
+            if (m_Async)
+            {
+                m_Status = Status.Success;
+                return;
+            }
+            DisplayManager.instance.onLastAnimationEnd = Continue;
+            m_Status = Status.Continue;
+        }
 
         protected override Status UpdateNode()
         {
-            DisplayManager.instance.RemoveCharacter(m_CharacterName);        
-            return Status.Success;
+           
+            return m_Status;
         }
     }
 }
